d00ba5378d09a836d3db75a5248ad364ba120706
YillonMask <yixinrui24@gmail.com>
2023-11-06 19:06:39 -0800

    lab9

---
+++ lab9/initialize.py
@@ -0,0 +1,21 @@
+"""
+    CS5001_5003 Fall 2023 SV
+    Lab09
+    Xinrui Yi
+"""
+
+
+def initialize():
+    """
+        this function creates and returns a dictionary that contains certain data
+    """
+    my_dic = {'Maria' : 'Assosiate Professor', 'John' : 'Clinical Instructor', 'Carla': 'Dean of Khoury' }
+    return my_dic
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
